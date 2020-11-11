d2aadd2c5757bce087f5c34ae3bdf91b31f96f9e
jayesh <jayeshsingh017@gmail.com>
2020-11-11 21:43:10 +0530

    dashboard added

--- app.js
+++ app.js
@@ -45,10 +45,13 @@ app.use("/form", require("./routes/feedbackform"));
     });
   });
 
+app.get("/",function(req,res)
+{
+  res.render("dash")
+});
 
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
